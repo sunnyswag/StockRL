82d720f3c708700d4acb609ff49d9f8bd599a939
sunnyswag <hhq126152@gmail.com>
2021-05-17 14:27:01 +0800

    delete ipynb_checkpoint_file

--- test/.ipynb_checkpoints/start_test-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-mkdir nohup
-nohup python -u RL_in_Finance_DDPG_Docker.py >./nohup/DDPG.log 2>&1 &
-nohup python -u RL_in_Finance_A2C_Docker.py >./nohup/A2C.log 2>&1 &
-#nohup python -u RL_in_Finance_PPO_Docker.py >./nohup/PPO.log 2>&1 &
-#nohup python -u RL_in_Finance_TD3_Docker.py >./nohup/TD3.log 2>&1 &
-#nohup python -u RL_in_Finance_SAC_Docker.py >./nohup/SAC.log 2>&1 &
--- test/.ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-sys.path.append("..")
-
-print(os.getcwd())
