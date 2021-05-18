e90c8cbf80a5fc59467e8541187f229b5c88db27
sunnyswag <hhq126152@gmail.com>
2021-05-18 12:17:50 +0800

    finish script module docs

--- script/tscode_transfer.py
+++ script/tscode_transfer.py
@@ -1,64 +1,58 @@
 import re
 import os
-import sys
 
-# 定义目录名
-path = "test"
-txt_path = "Index_TXT"
-txt_transfer_path = txt_path + "_Transfer"
-os.chdir(path)
+def creat_dir(txt_transfer_dir: str) -> None:
+    """创建转换后的目录"""
+    if not os.path.exists(txt_transfer_dir):
+        os.makedirs(txt_transfer_dir)
+        print("创建 {} 目录成功!".format(txt_transfer_dir))
+    else:
+        print(" {} 目录已存在!".format(txt_transfer_dir))
 
-# 创建转换后的目录
-if not os.path.exists(txt_transfer_path):
-    os.makedirs(txt_transfer_path)
-    print("创建 {} 目录成功!".format(txt_transfer_path))
-else:
-    print(" {} 目录已存在!".format(txt_transfer_path))
+def transfer(
+        txt_dir: str, transfer_dir: str, file_name: str
+    ) -> None:
+    """将原始股票代码批量修改成可以被 Tushare API 识别的股票代码
 
-def transfer(txt_dir, transfer_dir, file_name):
-    """使用 transfer 函数将原始股票代码批量修改成可以被 Tushare API 识别的股票代码
     Attributes
-    ----------
-        txt_dir : str
-            原始文件存放的目录
-        transfer_dir : str
-            修改后的文件存放的目录
-        file_name : str
-            原始文件的文件名
-    
-    Return
-    ------
-        None
+        txt_dir: 原始文件存放的目录
+        transfer_dir: 修改后的文件存放的目录
+        file_name: 原始文件的文件名
     """
-    test = []
-    file_name_sub = re.findall(r"(.+?).txt", file_name)[0]
-    test.append("    \"" + str(file_name_sub) + "\":[") # 第一行添加属于某个指数的标识, 生成为 dic 数据类型
 
-    cur_dir = os.path.join(txt_dir, file_name)
-    new_dir = os.path.join(transfer_dir, file_name_sub + "_Transferred.txt")
-    with open(cur_dir, "r") as f:
+    transferd = []
+    file_name_sub = re.findall(r"(.+?).txt", file_name)[0]
+    # 第一行添加该指数的标识
+    transferd.append(str(file_name_sub) + " = [") 
+
+    read_dir = os.path.join(txt_dir, file_name)
+    write_dir = os.path.join(transfer_dir, file_name_sub + "_Transferred.txt")
+    
+    with open(read_dir, "r") as f:
         for line in f.readlines():
             line = line.strip('\n')
             if line[0] == '6':
-                test.append("    \"" + str(line) + ".SH" + "\"")
+                transferd.append("    \"" + str(line) + ".SH" + "\"")
             else:
-                test.append("    \"" + str(line) + ".SZ" + "\"")
+                transferd.append("    \"" + str(line) + ".SZ" + "\"")
 
-    with open(new_dir, "w") as f:
+    with open(write_dir, "w") as f:
         # 第一行和最后一行单独处理
-        f.write(test[0])
+        f.write(transferd[0])
         f.write('\n')
 
-        for i in range(1, len(test) - 1):
-            f.write(test[i] + ',')
+        for i in range(1, len(transferd) - 1):
+            f.write(transferd[i] + ',')
             f.write('\n')
         
-        f.write(test[-1] + "]")
+        f.write(transferd[-1] + "]")
 
-txt_list = os.listdir(txt_path)
-for txt in txt_list:
-    transfer(txt_dir=txt_path, transfer_dir=txt_transfer_path, file_name=txt)
+if __name__ == "__main__":
+    txt_dir = "Index_TXT"
+    txt_transfer_dir = txt_dir + "_Transfer"
 
-# cwd = os.getcwd()
-# files = os.listdir(cwd)
-# print(cwd, files)
+    creat_dir(txt_transfer_dir)
+
+    txt_list = os.listdir(txt_dir)
+    for txt in txt_list:
+        transfer(txt_dir=txt_dir, transfer_dir=txt_transfer_dir, file_name=txt)
