bf0b6da93973676b5d6984a08ce6e4b09f7b8a85
sunnyswag <hhq126152@gmail.com>
2021-05-18 17:03:26 +0800

    update learn readme

---
+++ learn/readme.md
@@ -0,0 +1,39 @@
+## 下载数据
+
+在终端中输入
+
+```shell
+python ./data.py
+```
+
+数据保存在 `./data_file` 中
+
+## 训练
+
+在终端中输入
+
+```shell
+mkdir nohup
+nohup python -u ./trainer.py -m 'a2c' -tts 200000 >./nohup/A2C.log 2>&1 &
+```
+
+**TIPS**：
+
+* 若未下载数据，运行 `./trainer.py` 将自动下载
+* 可运行 [./start_train.sh](./start_train.sh) 批量化操作
+* 运行日志保存在 `./nohup` 中
+* 运行完成后的模型保存在 `./train_file` 中
+
+## 回测
+
+在终端中输入
+
+```shell
+python ./trader.py -m 'a2c'
+```
+
+**TIPS**：
+
+* 若未找到 `.model` 文件，可以在 [release]() 中进行下载
+* 回测数据保存在 `./trade_file` 中
+
--- learn/start_train.sh
+++ learn/start_train.sh
@@ -1,6 +1,6 @@
 mkdir nohup
-nohup python -u trainer.py -m 'a2c' -tts 200000 >./nohup/A2C.log 2>&1 &
-# nohup python -u trainer.py -m 'ddpg' -tts 200000 >./nohup/DDPG.log 2>&1 &
-# nohup python -u trainer.py -m 'ppo' -tts 200000 >./nohup/PPO.log 2>&1 &
-nohup python -u trainer.py -m 'td3' -tts 200000 >./nohup/TD3.log 2>&1 &
-# nohup python -u trainer.py -m 'sac' -tts 200000 >./nohup/SAC.log 2>&1 &
+nohup python -u ./trainer.py -m 'a2c' -tts 200000 >./nohup/A2C.log 2>&1 &
+# nohup python -u ./trainer.py -m 'ddpg' -tts 200000 >./nohup/DDPG.log 2>&1 &
+# nohup python -u ./trainer.py -m 'ppo' -tts 200000 >./nohup/PPO.log 2>&1 &
+nohup python -u ./trainer.py -m 'td3' -tts 200000 >./nohup/TD3.log 2>&1 &
+# nohup python -u ./trainer.py -m 'sac' -tts 200000 >./nohup/SAC.log 2>&1 &
